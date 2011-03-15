13ec552322affe8ef5af8a10085dae709757d1e9
Kevin <kevin@ksturner.com>
2011-03-15 00:01:18 -0500

    added in some images that were missing

--- package_darwin.sh
+++ package_darwin.sh
@@ -9,4 +9,7 @@ zip runtime.zip \
 	i/help-browser.png \
 	i/address-book-new.png \
 	i/preferences-system.png \
-	i/logo01.icns
+	i/logo01.icns \
+    i/logo01_32x32.png \
+    i/logo01_64x64.png \
+    i/logo01_96x96.png
